b37c3fa5fbc7612abd33d3704e625255fbcce7ef
maverickblancaver <blancaver63@gmail.com>
2024-06-20 19:49:57 +0800

    week01

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,5 @@
 // getdates.js
 document.addEventListener('DOMContentLoaded', function() {
-    // Set current year in the footer
     document.getElementById('currentyear').textContent = new Date().getFullYear();
-    
-    // Set last modified date in the footer
-    var lastModifiedDate = new Date(document.lastModified);
-    var options = { year: 'numeric', month: 'long', day: 'numeric' };
-    document.getElementById('lastModified').textContent = 'Last Modified: ' + lastModifiedDate.toLocaleDateString(undefined, options);
+    document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
 });
