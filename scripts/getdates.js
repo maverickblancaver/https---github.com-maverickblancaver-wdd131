10d160fa7c8a8b5782c8787027d428b0e76f0444
maverickblancaver <blancaver63@gmail.com>
2024-06-20 19:19:03 +0800

    week01

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// getdates.js
+
+// Update copyright year dynamically
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Update last modified date dynamically
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
