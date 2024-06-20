7d83d893b110b083de0b22014f03daa5813e713a
maverickblancaver <blancaver63@gmail.com>
2024-06-20 19:44:38 +0800

    week01

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,10 @@
 // getdates.js
-
-// Update copyright year dynamically
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-
-// Update last modified date dynamically
-document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
+document.addEventListener('DOMContentLoaded', function() {
+    // Set current year in the footer
+    document.getElementById('currentyear').textContent = new Date().getFullYear();
+    
+    // Set last modified date in the footer
+    var lastModifiedDate = new Date(document.lastModified);
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    document.getElementById('lastModified').textContent = 'Last Modified: ' + lastModifiedDate.toLocaleDateString(undefined, options);
+});
